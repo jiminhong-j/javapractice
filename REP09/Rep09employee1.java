6580c06322f395de39aa2ccf4c81ed44bcd0ed93
Jimin <jiminh101@gmail.com>
2026-05-26 22:53:27 +0900

    rep09-1

--- REP09/Rep09employee1.java
+++ REP09/Rep09employee1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 class Person {
     protected String name;
 
+    // 생성자 + 기본 생성자 모두 구현
     public Person(){
         name == "";
     }
@@ -26,6 +27,7 @@ class Employee extends Person {
     private String ssn;
     private double salary;
 
+    // 생성자 + 기본 생성자 모두 구현
     public Employee(){
         super();
         ssn = "";
@@ -37,13 +39,54 @@ class Employee extends Person {
         this.ssn = ssn;
         this.salary = salary;
     }
+    
+    public String getSsn(){
+        return ssn;
+    }
+
+    public void setSsn(String ssn){
+        this.ssn = ssn;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
 }
 
-// 생성자 + 기본 생성자 모두 구현
-
-// 최대 10명의 직원 정보 저장할 수 있는 Employee 배열 생성
-
-// 사용자로부터 직원수 입력받기
-// 각 직원에 대해 name, ssn, 급여 정보 입력받아 배열에 저장
-
 // 예외처리 구현 X
+public class Rep09employee1 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        
+        // 최대 10명의 직원 정보 저장할 수 있는 Employee 배열 생성
+        Employee[] employee = new Employee[10];
+
+        // 사용자로부터 직원수 입력받기
+        int count;
+        System.out.print("How many employee? (max 10): ");
+        count = s.nextInt();
+        s.nextLine();
+
+        // 각 직원에 대해 name, ssn, 급여 정보 입력받아 배열에 저장
+        for(int i = 0; i < count; i++){
+            System.out.println();
+            System.out.println("Entering data for employee " + (i+1));
+            System.out.println("Enter name: ");
+            String name = s.nextLine();
+            System.out.println("Enter SSN (no dashes or spaces): ");
+            String ssn = s.nextLine();
+            System.out.println("Enter salary: ");
+            double salary = s.nextDouble();
+            s.nextLine();
+
+            employee[i] = new Employee(name, ssn, salary);
+        }
+        s.close();
+    }
+}
+
