7c81ebfa440a7ae9a2049670182c43ea33b6ea4a
Jimin <jiminh101@gmail.com>
2026-06-03 16:58:08 +0900

    handshake1

---
+++ REP11/Handshake1.java
@@ -0,0 +1,16 @@
+
+public class Handshake1 {
+    public static int handshake(int n){
+        if(n <= 1) return 0;
+        else if(n == 2) return 1;
+        else {
+            return handshake(n - 1) + (n - 1);
+        }
+    }
+
+    public static void main(String args) {
+        int n = 1;
+
+        System.out.println("Total handshakes for " + n + " people: " + handshake(n));
+    }
+}
