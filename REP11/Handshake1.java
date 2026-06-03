e2502de875ecb3a415fc41766ad73415a1712699
Jimin <jiminh101@gmail.com>
2026-06-03 17:13:03 +0900

    String[]

--- REP11/Handshake1.java
+++ REP11/Handshake1.java
@@ -8,8 +8,8 @@ public class Handshake1 {
         }
     }
 
-    public static void main(String args) {
-        int n = 1;
+    public static void main(String[] args) {
+        int n = 5;
 
         System.out.println("Total handshakes for " + n + " people: " + handshake(n));
     }
